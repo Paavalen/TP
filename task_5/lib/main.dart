98fd5ab63b0e09f6398704671433a2d44d4c4ad6
Paavalen <p.lingachetti@gmail.com>
2023-10-22 17:30:05 +0400

    add Theme and style

--- task_5/lib/main.dart
+++ task_5/lib/main.dart
@@ -67,6 +67,19 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(appState.current.asLowerCase);
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(appState.current.asLowerCase, style: style),
+      ),
+    );
   }
 }
